4c7d30e1f81249d154e16eda2f96888a61f715c3
nikkurent <nik.kurent1@gmail.com>
2024-02-20 11:10:59 +0100

    web fix

--- lib/pages/qr_code_and_wallet_list_page.dart
+++ lib/pages/qr_code_and_wallet_list_page.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:walletconnect_modal_flutter/constants/constants.dart';
-import 'package:walletconnect_modal_flutter/models/listings.dart';
-import 'package:walletconnect_modal_flutter/pages/wallet_list_long_page.dart';
-import 'package:walletconnect_modal_flutter/services/explorer/explorer_service_singleton.dart';
-import 'package:walletconnect_modal_flutter/services/utils/widget_stack/widget_stack_singleton.dart';
 import 'package:walletconnect_modal_flutter/services/walletconnect_modal/i_walletconnect_modal_service.dart';
-import 'package:walletconnect_modal_flutter/widgets/grid_list/grid_list.dart';
-import 'package:walletconnect_modal_flutter/widgets/grid_list/grid_list_wallet_item.dart';
 import 'package:walletconnect_modal_flutter/widgets/qr_code_widget.dart';
 import 'package:walletconnect_modal_flutter/widgets/walletconnect_modal_navbar.dart';
 import 'package:walletconnect_modal_flutter/widgets/walletconnect_modal_navbar_title.dart';
@@ -32,26 +26,26 @@ class QRCodeAndWalletListPage extends StatelessWidget {
             service: service,
             logoPath: 'assets/walletconnect_logo_white.png',
           ),
-          GridList(
-            state: GridListState.extraShort,
-            provider: explorerService.instance!,
-            viewLongList: () {
-              widgetStack.instance.add(
-                const WalletListLongPage(),
-              );
-            },
-            onSelect: (WalletData data) {
-              service.connectWallet(
-                walletData: data,
-              );
-            },
-            createListItem: (info, iconSize) {
-              return GridListWalletItem(
-                listItem: info,
-                imageSize: iconSize,
-              );
-            },
-          ),
+          // GridList(
+          //   state: GridListState.extraShort,
+          //   provider: explorerService.instance!,
+          //   viewLongList: () {
+          //     widgetStack.instance.add(
+          //       const WalletListLongPage(),
+          //     );
+          //   },
+          //   onSelect: (WalletData data) {
+          //     service.connectWallet(
+          //       walletData: data,
+          //     );
+          //   },
+          //   createListItem: (info, iconSize) {
+          //     return GridListWalletItem(
+          //       listItem: info,
+          //       imageSize: iconSize,
+          //     );
+          //   },
+          // ),
         ],
       ),
     );
